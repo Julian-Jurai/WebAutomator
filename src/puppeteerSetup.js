2dea649573f91dff656843cce11cfb38f19976b6
Julian <andrewjurai@gmail.com>
2019-04-13 09:50:54 -0400

    Re-enable spoof + Hook into browser logs

--- src/puppeteerSetup.js
+++ src/puppeteerSetup.js
@@ -5,6 +5,14 @@ const TIMEOUT = 90 * 1000;
 export const puppeteerInit = async () => {
   const browser = await puppeteer.launch({ headless: !process.env.DEBUG_MODE });
   const page = await browser.newPage();
+
+  //Hook Into Headless Browser Console Message
+  page.on("console", consoleObj => {
+    const msg = consoleObj.text();
+    const isUserDefinedMessage = msg.slice(0, 2) == "GM";
+    if (isUserDefinedMessage) console.log(msg);
+  });
+
   page.setDefaultTimeout(TIMEOUT);
   return { browser, page };
 };
--- src/utils/spoof.js
+++ src/utils/spoof.js
@@ -1,14 +1,14 @@
 import { exec } from "child_process";
 
 export default () => {
-  // if (process.env.PASSWORD) {
-  //   const cmd = `sudo -S <<< "${process.env.PASSWORD}" spoof randomize en0`;
-  //   return new Promise((resolve, reject) => {
-  //     console.log("Spoofed ✅");
-  //     exec(cmd, {}, (error, stdout, stderr) => {
-  //       setTimeout(resolve, 2000);
-  //       error && console.error(error, stdout, stderr, "❌");
-  //     });
-  //   });
-  // }
+  if (process.env.PASSWORD) {
+    const cmd = `sudo -S <<< "${process.env.PASSWORD}" spoof randomize en0`;
+    return new Promise((resolve, reject) => {
+      console.log("Spoofed ✅");
+      exec(cmd, {}, (error, stdout, stderr) => {
+        setTimeout(resolve, 2000);
+        error && console.error(error, stdout, stderr, "❌");
+      });
+    });
+  }
 };
